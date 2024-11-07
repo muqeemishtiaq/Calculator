33631fd491cb16b63fe400c795a5c1e0277a3ac3
muqeemishtiaq <126596373+muqeemishtiaq@users.noreply.github.com>
2024-11-07 09:53:29 +0500

    initial commit

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-let string = "0";  // Start with "0" initially
+let string = "0"; 
 let buttons = document.querySelectorAll('.button');
 
 Array.from(buttons).forEach((button) => {
    button.addEventListener('click', (e) => { 
       if (e.target.innerHTML === "=") {
-         // Evaluate the expression
+       
          string = eval(string);
          document.querySelector('input').value = string;
       } 
       else if (e.target.innerHTML === "C") { 
-         // Clear the input and set it to "0"
+      
          string = "0";
          document.querySelector('input').value = string;
       } 
       else {
-         // If the current display is "0", replace it with the new input
+        
          if (string === "0") {
             string = e.target.innerHTML;
          } else { 
-            // Otherwise, append the clicked button's text to the string
+          
             string += e.target.innerHTML;
          }
          document.querySelector('input').value = string;
